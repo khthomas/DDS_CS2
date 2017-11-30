11964258744574ab5a8ad71041a7eab2ebad7195
mrega1 <31941289+mrega1@users.noreply.github.com>
2017-11-29 19:46:48 -0500

    Code to question 5 E and D

---
+++ Code/Question5_d_e.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+
+#scatterplot income vs age
+ggplot(subset(Procrastination,!is.na(AnnualIncome)&!is.na(Age)&!is.na(Gender)),aes(x=Age,y=AnnualIncome,shape=Gender,color=Gender))+geom_point()+xlab("Age")+ylab("Annual Income in Dollars")+ggtitle("Annual Income vs Age")+theme(plot.title = element_text(hjust=0.5))
+
+#create smaller data set for regression model
+income_age_data<-data.frame(Procrastination$Age,Procrastination$Gender,Procrastination$AnnualIncome)
+names(income_age_data)<-c("Age","Gender","Income")
+income_age_data$male<-ifelse(income_age_data$Gender=="Male",1,0)
+
+#correlation of age and income
+cor.test(Procrastination$Age,Procrastination$AnnualIncome,method="pearson",use = "complete.obs")
+
+#create linear regression model using just age then age and gender to predict income
+income_age_model<-lm(Income~Age,data=income_age_data)
+summary(income_age_model)
+income_age_model2<-lm(Income~male+Age,data=income_age_data)
+summary(income_age_model2)
+
+
+#scatterplot life satisfaction vs hdi
+ggplot(Procrastination,aes(x=Procrastination$`2016_Est_HDI`,y=SWLSPMean,shape=Gender,color=Gender))+geom_point()+xlab("Human Development Rating")+ylab("Life Statisfaction Rating (1-5(Highest))")+ggtitle("Life Statisfaction vs Human Development Rating")+theme(plot.title = element_text(hjust=0.5))
+
+#create smaller data set for regression model
+ls_hdi_data<-data.frame(as.numeric(Procrastination$`2016_Est_HDI`),Procrastination$Gender,Procrastination$SWLSPMean)
+names(ls_hdi_data)<-c("HDI","Gender","Life_Satisfaction")
+ls_hdi_data$male<-ifelse(ls_hdi_data$Gender=="Male",1,0)
+
+#correlation of hdi and life statisfaction
+cor.test(as.numeric(Procrastination$`2016_Est_HDI`),Procrastination$SWLSPMean,method="pearson",use = "complete.obs")
+
+#create linear regression model using just HDI then HDI and gender to predict life statisfaction
+ls_hdi_model<-lm(Life_Satisfaction~HDI,data=ls_hdi_data)
+summary(ls_hdi_model)
+ls_hdi_model2<-lm(Life_Satisfaction~HDI+male,data=ls_hdi_data)
+summary(ls_hdi_model2)
